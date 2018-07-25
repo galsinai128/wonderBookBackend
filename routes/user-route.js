ab6cb55b094474dd7f69da51bd429e48ea58a97d
gal <galsinay128@gmail.com>
2018-07-25 12:50:52 +0300

    user service and route

---
+++ routes/user-route.js
@@ -0,0 +1,22 @@
+const userService = require('../services/user-service')
+// const reviewService = require('../services/review-service')
+const BASE = '/user'
+
+function addRoutes(app) {
+    app.get(BASE, (req, res) => {
+        userService.query()
+            .then(users => res.json(users))
+    })
+    app.get(`${BASE}/:id`, (req, res) => {
+        const userId = req.params.id
+        userService.getById(userId)
+            // reviewService.query({ userId })
+            .then((user) => {
+            // console.log({user})
+                res.json(user)
+        })
+    })
+}
+
+
+module.exports = addRoutes;
