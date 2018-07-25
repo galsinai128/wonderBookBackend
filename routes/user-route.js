043072d1a7c8da15db33b5cba3cf12d4f6f113fc
omers <omer3026@gmail.com>
2018-07-25 13:18:43 +0300

    user login and sign up code now in comment

--- routes/user-route.js
+++ routes/user-route.js
@@ -12,10 +12,45 @@ function addRoutes(app) {
         userService.getById(userId)
             // reviewService.query({ userId })
             .then((user) => {
-            // console.log({user})
+                // console.log({user})
                 res.json(user)
-        })
+            })
     })
+
+    // app.post(`/signup`, (req, res) => {
+    //     const user = req.body;
+    //     UserService.addUser(user)
+    //         .then(addedUser => {
+    //             console.log(addedUser)
+    //             req.session.loggedinUser = addedUser
+    //             res.json(addedUser)})
+    //         .catch(err => {
+    //             console.log(err)
+    //             res.status(500).send('Could not add USER')
+    //         })
+    // }),
+    // app.post('/checkLogin', (req, res) => {
+    //     const credentials = req.body
+    //     console.log(credentials,'user')
+    //     UserService.checkLogin(credentials)
+    //         .then(user => {
+    //             var user = {...user}
+    //             delete user.password;
+    //             req.session.loggedinUser = user
+    //             console.log('user loggin',req.session.loggedinUser)
+    //             res.json(user)
+    //         })
+    //         .catch(err => res.status(401).send('Wrong user/pass'))
+    // });
+
+    // app.post(`/logout`, (req, res) => {
+    //     req.session.loggedinUser = null;
+    //     console.log('Loggedout',req.session.loggedinUser)
+
+    //     res.end('Loggedout!');
+    // });
+
+
 }
 
 
