d7fbf6f3df86dd83543748766278bbe99a7f50b3
Itzik Turjeman <itzikit@gmail.com>
2018-07-26 13:06:13 +0300

    update data structure

---
+++ data/newDataItzik.js
@@ -0,0 +1,173 @@
+[
+    {
+        title :'Where’s My Sandwich?',
+        author: 'Michelle Dennise',
+        illustrator: 'Omer Sela',
+        categories: ['Age 2-5 Years', 'All FKB Books', 'Animals', 'Dogs', 'Learning to Read', 'Toddlers'],
+        descrition: 'Where’s my Sandwich is a delightful story for young children’s about a picnic in the park and a mysterious missing sandwich. The story features repetition and simple language making it great for learning to read age children. This is another great free children’s story provided by Michelle Dennise.',
+        rating : 4.15,
+        views : 1381,
+        numOfPages : 7,
+        audio : '',
+        pages : [
+            {
+                txt : 'Where’s My Sandwich?',
+                time : 0,
+                img : './sandwich/cover.jpg'
+            },
+            {
+                txt : 'I am very excited today. Archie and I are going to the nature reserve for a picnic. We love the nature reserve. There are many animals to see and a pretty river and open spaces to run and play.',
+                time : 1,
+                img : './sandwich/page1.jpg'
+            },
+            {
+                txt : 'I have packed fruit, lemonade, a cake and best of all - sandwiches. Archie loves my sandwiches.',
+                time : 2,
+                img : './sandwich/page2.jpg'
+            },
+            {
+                txt : 'When we arrived at the nature reserve I started to set up the picnic while Archie chased some birds.',
+                time : 3,
+                img : './sandwich/page3.jpg'
+            },
+            {
+                txt : 'When everything was ready I called to Archie and sat down to eat...But my sandwich was missing! “Where\’s my sandwich?” I said as I looked around.',
+                time : 4,
+                img : './sandwich/page4.jpg'
+            },
+            {
+                txt : 'Then I saw it. Crumbs. A crumb trail. Interesting! I decided to follow the trail to see where it leads. Maybe I will find my sandwich...or the one who took it',
+                time : 5,
+                img : './sandwich/page5.jpg'
+            },
+            {
+                txt : 'This crumb trail was following the walkway through the bush. It appeared to be leading back to the picnic area.',
+                time : 6,
+                img : './sandwich/page6.jpg'
+            },
+            {
+                txt : 'Back at the picnic area I found a very guilty looking puppy. “Archie, where\’s my sandwich?” “I’m sorry Nellie, I ate it all up. You know I love your sandwiches,” said Archie',
+                time : 7,
+                img : './sandwich/page7.jpg'
+            },
+
+        ],
+ 
+        reviews : [],
+        status: 'done',
+        createdAt: 1532597037875,
+ 
+    },
+    {
+        title :'Walking Together',
+        author: 'Madhav Chavan',
+        illustrator: 'Rijuta Ghate',
+        categories: ['Beginner English', 'Behaviour', 'Creative Commons', 'Family', 'Toddlers'],
+        descrition: 'Walking Together tells a simple rhyming story about walking together to the clinic. A beautiful story for young children who may be afraid of going to the Doctor. Another great creative commons book by Bookdash!',
+        rating : 3.46,
+        views : 4904,
+        numOfPages : 7,
+        audio : '',
+        pages : [
+            {
+                txt : 'Walking Together',
+                time : 0,
+                coverImg : './walkingTogether/cover.jpg',
+            },
+            {
+                txt : 'When we go walking and the road is long, we’ll walk together singing a song.',
+                time : 1,
+                coverImg : './walkingTogether/page1.jpg',
+            },
+            {
+                txt :  'I am with you and you are with me. A journey is funwith our family.',
+                time : 2,
+                coverImg : './walkingTogether/page2.jpg',
+            },
+            {
+                txt : 'I’ll keep you cosy like a mother hen. She keeps her chicks warm under her wing.',
+                time : 3,
+                coverImg : './walkingTogether/page3.jpg',
+            },
+            {
+                txt : 'If the rain starts to fall down from the sky, like that good hat I’ll keep your face dry.',
+                time : 4,
+                coverImg : './walkingTogether/page4.jpg',
+            },
+            {
+                txt : 'If storm clouds get angry and grumble and moan, be brave, why cry? You’re not alone.',
+                time : 5,
+                coverImg : './walkingTogether/page5.jpg',
+            },
+            {
+                txt : 'We’re walking together so we never get bored. Look up! Look around! See where we’ve explored!',
+                time : 6,
+                coverImg : './walkingTogether/page6.jpg',
+            },
+            {
+                txt : 'If you see something that’s funny to see, I don’t want to miss it. Please share it with me!',
+                time : 7,
+                coverImg : './walkingTogether/page7.jpg',
+            },
+        ],
+ 
+        reviews : [],
+        status: 'done',
+        createdAt: 1532597037875,
+ 
+    },
+    {
+        title : 'Monster Kingdom',
+        author : 'Gal Sinai',
+        illustrator: 'Itzik Turjeman',
+        categories: ['Beginner English', 'Behaviour', 'Creative Commons', 'Family'],
+        description : 'The tiny, multi-colored bean-shaped bears of Beandom are under attack by a monster. Even the Kings wisest advisors seem unable find a solution. Who will save Beandom? Can an ordinary tiny bear step forward with a plan that works? The message of the story is: Follow your convictions even when others think differently.',
+        rating : 4.62,
+        views : 3463,
+        numOfPages : 0,
+        audio : '',
+        pages : [
+            {
+                txt : 'Monster Kingdom',
+                time : 0,
+                img : './kingdom/cover.jpg'
+            },
+            {
+                txt : 'There was a monster in the kingdom that was so big that it made the earth tremble whenever it walked.',
+                time : 1,
+                img : './kingdom/page1.jpg'
+            },
+            {
+                txt : 'The monster quickly ate the food, but it would be back the next day. The kingdom needed a better plan.',
+                time : 2,
+                img : './kingdom/page2.jpg'
+            },
+            {
+                txt : 'Fortunately, Sandy went to the king and presented a clever plan for handling the monster.',
+                time : 3,
+                img : './kingdom/page3.jpg'
+            },
+            {
+                txt : 'There was a monster in the kingdom that was so big that it made the earth tremble whenever it walked.',
+                time : 4,
+                img : './kingdom/page4.jpg'
+            },
+            {
+                txt : 'The monster quickly ate the food, but it would be back the next day. The kingdom needed a better plan.',
+                time : 5,
+                img : './kingdom/page5.jpg'
+            },
+            {
+                txt : 'Fortunately, Sandy went to the king and presented a clever plan for handling the monster.',
+                time : 6,
+                img : './kingdom/page6.jpg'
+            },
+
+        ],
+ 
+        reviews : [],
+        status: 'done',
+        createdAt: 1532597037875,
+ 
+    },  
+]
